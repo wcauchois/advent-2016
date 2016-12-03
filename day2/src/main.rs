0ea471a3689090a4fccc43bcccb947f9382de4d5
Bill Cauchois <wcauchois@gmail.com>
2016-12-02 23:46:37 -0500

    adapt day2 for second star

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -41,6 +41,7 @@ fn vec_add(v1: Vec2, v2: Vec2) -> Vec2 {
   }
 }
 
+/*
 fn vec_clamp(v: Vec2, min: Vec2, max: Vec2) -> Vec2 {
   match (v, min, max) {
     ((x, y), (min_x, min_y), (max_x, max_y)) => {
@@ -51,22 +52,42 @@ fn vec_clamp(v: Vec2, min: Vec2, max: Vec2) -> Vec2 {
     },
   }
 }
+*/
 
-fn pos_to_key(pos: Vec2) -> String {
-  match pos {
-    (x, y) => (y * 3 + x + 1).to_string()
-  }
+fn pos_to_key(pos: Vec2) -> Option<String> {
+  //fukit
+  (match pos {
+    (2, 0) => Some("1"),
+
+    (1, 1) => Some("2"),
+    (2, 1) => Some("3"),
+    (3, 1) => Some("4"),
+
+    (0, 2) => Some("5"),
+    (1, 2) => Some("6"),
+    (2, 2) => Some("7"),
+    (3, 2) => Some("8"),
+    (4, 2) => Some("9"),
+
+    (1, 3) => Some("A"),
+    (2, 3) => Some("B"),
+    (3, 3) => Some("C"),
+
+    (2, 4) => Some("D"),
+
+    _ => None,
+  }).map(|s| s.to_string())
 }
 
 fn main() {
   let f = File::open("input.txt").unwrap();
   let file = BufReader::new(&f);
-  // (0, 0)
-  //       1 2 3
-  //       4 5 6
-  //       7 8 9
-  //            (2, 2)
-  let mut pos = (1, 1);
+  //     1
+  //   2 3 4
+  // 5 6 7 8 9
+  //   A B C
+  //     D
+  let mut pos = (0, 2);
   let mut code = String::from("");
   for line in file.lines() {
     let l = line.unwrap();
@@ -74,9 +95,13 @@ fn main() {
       .map(|c| Direction::parse(&c.to_string()))
       .collect::<Vec<Direction>>();
     for dir in dirs {
-      pos = vec_clamp(vec_add(pos, dir.to_vec()), (0, 0), (2, 2));
+      let potential_new_pos = vec_add(pos, dir.to_vec());
+      let is_legal = pos_to_key(potential_new_pos).is_some();
+      if is_legal {
+        pos = potential_new_pos;
+      }
     }
-    code.push_str(&pos_to_key(pos));
+    code.push_str(&pos_to_key(pos).unwrap());
   }
   println!("{}", code);
 }
