318d7c44d731b9fb83cb8ab287369e8199a3744f
Bill Cauchois <wcauchois@gmail.com>
2016-12-05 00:16:12 -0500

    part2

--- day5/main.py
+++ day5/main.py
@@ -6,16 +6,25 @@ def md5(s):
   return m.hexdigest()
 
 door_id = 'ugkcyxxp'
+#door_id = 'abc'
 index = 0
-code = ''
+#code = ''
+
+pos_dict = {}
+
 while True:
   to_hash = door_id + str(index)
   h = md5(to_hash)
-  if h[0:5] == '00000':
-    code += h[5]
-    if len(code) == 8:
+  if h[0:5] == '00000' and h[5] in '01234567':
+    if int(h[5]) not in pos_dict:
+      pos_dict[int(h[5])] = h[6]
+    if all([i in pos_dict for i in range(0, 8)]):
       break
   index += 1
     
-print code
-
+print pos_dict
+print
+buf = ''
+for i in range(0, 8):
+  buf += pos_dict[i]
+print buf
